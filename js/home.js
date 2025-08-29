53ca0348539029c6458ba351b625e258c2151c10
mariana <eumarianamota@gmail.com>
2025-08-29 09:38:15 -0300

    organization of files

--- js/home.js
+++ js/home.js
@@ -7,13 +7,13 @@ form.addEventListener('submit',  (event) => {
     event.preventDefault()
 
     if (user.value === 'administrador' && password.value === "1234") {
-        window.location.href = "manager.html"
+        window.location.href = "./adm/adm.html"
     } else if (user.value === "recepcao" && password.value === "1234") {
-        window.location.href = "reception.html"
+        window.location.href = "reception/reception.html"
     } else if (user.value === "triagem" && password.value === "1234") {
-        window.location.href = "triage.html"
+        window.location.href = "screening/screening.html"
     } else if (user.value === "medico" && password.value === "1234") {
-        window.location.href = "doctor.html"
+        window.location.href = "doctor/doctor.html"
     } else {
         errorMessage.textContent = "Usuário ou Senha inválidos!"
     }
